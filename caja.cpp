59b4ad826c46c497a6d257fb3cafb1846fe3eeda
Kevin Pizarro <kevin.pizarroa@sansano.usm.cl>
2021-07-27 21:17:13 -0400

    caja.cpp comentado

--- caja.cpp
+++ caja.cpp
@@ -1,6 +1,10 @@
 #include "caja.h"
 #include "ui_caja.h"
 
+/**
+ * @brief Caja::Caja: Constructor de la clase
+ * @param parent: Ventana principal a la que pertenece
+ */
 Caja::Caja(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Caja)
@@ -14,17 +18,27 @@ Caja::Caja(QWidget *parent) :
     this->fecha->setDate(QDate::currentDate());
 }
 
+/**
+ * @brief Caja::~Caja: Destructor de la clase
+ */
 Caja::~Caja()
 {
     delete ui;
 }
 
+/**
+ * @brief Caja::on_pushButton_clicked: Slot correspondiente a cuando es presionado el boton Revisar
+ */
 void Caja::on_pushButton_clicked()
 {
-
+    //TODO: Por implementar
 }
 
+/**
+ * @brief Caja::closeEvent: Metodo que maneja el cierre de la ventana, modificado para que no destruya el objeto
+ * @param event: Evento de cierre de ventana
+ */
 void Caja::closeEvent (QCloseEvent *event){
-    event->ignore();
-    this->hide();
+    event->ignore();//Ignora el cierre de ventana
+    this->hide();   //Esconde la ventana
 }
