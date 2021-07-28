71d65714d05d86e10aef305ad7b8ca966613016e
Migelgo <miguel.andrader@sansano.usm.cl>
2021-07-27 20:31:49 -0400

    primer draft de mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <ventas.h>
+#include <compras.h>
+#include <inventario.h>
+#include <producto.h>
+#include <caja.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +23,24 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    bool vflag, cflag;
+    QTimer *timer;
+    Ventas *ventas;
+    Compras *compras;
+    Inventario *inventario;
+    Producto *producto;
+    Caja *caja;
+
+public slots:
+    void change_enabled_compras();
+    void change_enabled_ventas();
+    void askhide();
+
+private slots:
+    void on_actionVentas_triggered();
+    void on_actionCompras_triggered();
+    void on_actionInventario_triggered();
+    void on_actionProductos_triggered();
+    void on_actionCja_triggered();
 };
 #endif // MAINWINDOW_H
