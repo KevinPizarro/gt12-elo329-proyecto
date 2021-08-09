ee586aaa41649d797ae12cd65da252fb2c40e18d
Migelgo <miguel.andrader@sansano.usm.cl>
2021-08-09 19:00:24 -0400

    Inventario funcional

--- inventario.cpp
+++ inventario.cpp
@@ -8,6 +8,8 @@ Inventario::Inventario(QWidget *parent) :
     ui->setupUi(this);
     this->limpiar = ui->limpiar;
     this->guardar = ui->Guardar;
+    database= new DbManager();
+    setInv();
 }
 
 Inventario::~Inventario()
@@ -18,3 +20,71 @@ void Inventario::closeEvent (QCloseEvent *event){
     event->ignore();
     this->hide();
 }
+
+void Inventario::setInv(){
+    QSqlQueryModel *model=new QSqlQueryModel();
+    model->setQuery("select * from Producto");
+    QSqlQuery query;
+    QSqlRecord row;
+    for(int i=0; i<model->rowCount(); i++){
+        row=model->record(i);
+        for(int j=0;j<3;j++){
+            ui->tableWidget->setItem(i,j,new QTableWidgetItem(row.value(j).toString()));
+            if(j == 0 || j == 1){
+                ui->tableWidget->item(i,j)->setFlags(ui->tableWidget->item(i,j)->flags() & ~Qt::ItemIsEditable);
+            }
+        }
+    }
+    ui->tableWidget->setColumnWidth(0,200);
+    ui->tableWidget->setColumnWidth(1,700);
+    ui->tableWidget->setColumnWidth(2,150);
+    ui->tableWidget->setFixedWidth(1100);
+    ui->tableWidget->setFixedHeight(400);
+    ui->tableWidget->adjustSize();
+    ui->tableWidget->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    ui->tableWidget->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+}
+
+void Inventario::on_Guardar_clicked(){
+    QSqlQueryModel *model=new QSqlQueryModel();
+    model->setQuery("select * from Producto");
+    QSqlQuery query;
+    int codigo;
+    int saldo;
+    QString c;
+    QString s;
+    for(int i=0; i < model->rowCount(); i++){
+        codigo = ui->tableWidget->item(i,0)->text().toInt();
+        saldo  = ui->tableWidget->item(i,2)->text().toInt();
+        c = QString::number(codigo);
+        s = QString::number(saldo);
+        query.prepare("update Producto SET [Saldo] = :saldo where Codigo = :codigo");
+        query.bindValue(":codigo",c);
+        query.bindValue(":saldo",s);
+        if(query.exec()){
+        }
+    }
+    QMessageBox msgBox;
+    msgBox.setWindowTitle("Guardado");
+    msgBox.setText("Cambios Guardados.");
+    msgBox.exec();
+}
+
+
+void Inventario::on_limpiar_clicked(){
+    QSqlQueryModel *model=new QSqlQueryModel();
+    model->setQuery("select * from Producto");
+    QSqlQuery query;
+    QSqlRecord row;
+    for(int i=0; i<model->rowCount(); i++){
+        row=model->record(i);
+        for(int j=0;j<3;j++){
+            ui->tableWidget->setItem(i,j,new QTableWidgetItem(row.value(j).toString()));
+            if(j == 0 || j == 1){
+                ui->tableWidget->item(i,j)->setFlags(ui->tableWidget->item(i,j)->flags() & ~Qt::ItemIsEditable);
+            }
+        }
+    }
+}
+
--- inventario.h
+++ inventario.h
@@ -1,9 +1,18 @@
 #ifndef INVENTARIO_H
 #define INVENTARIO_H
-
+#include "dbmanager.h"
 #include <QWidget>
 #include <QCloseEvent>
 #include <QPushButton>
+#include <QMainWindow>
+#include <QTableView>
+#include <QSqlQueryModel>
+#include <QDebug>
+#include <QTableWidget>
+#include <QSqlRecord>
+#include <QTableWidgetItem>
+#include <QSqlQuery>
+#include <QMessageBox>
 
 namespace Ui {
 class Inventario;
@@ -17,8 +26,15 @@ public:
     explicit Inventario(QWidget *parent = nullptr);
     ~Inventario();
     void closeEvent (QCloseEvent *event);
+    void setInv();
     QPushButton *guardar;
     QPushButton *limpiar;
+    DbManager *database;
+
+private slots:
+    void on_Guardar_clicked();
+
+    void on_limpiar_clicked();
 
 private:
     Ui::Inventario *ui;
