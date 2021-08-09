2db1ee769a5679bdafe6dfc000f22dc9a21a30a2
Manuel Cruces <manuel.cruces@sansano.usm.cl>
2021-08-09 23:55:14 +0000

    compras.h actualizado

--- compras.h
+++ compras.h
@@ -1,12 +1,22 @@
 #ifndef COMPRAS_H
 #define COMPRAS_H
-
+#include "dbmanager.h"
 #include <QWidget>
 #include <QLineEdit>
 #include <QDateEdit>
 #include <QCloseEvent>
 #include <QCheckBox>
 #include <QPushButton>
+#include <QTableView>
+#include <QSqlQueryModel>
+#include <QDebug>
+#include <QTableWidget>
+#include <QSqlRecord>
+#include <QTableWidgetItem>
+#include <QSqlQuery>
+#include <QVector>
+#include <QMessageBox>
+#include <QDate>
 
 namespace Ui {
 class Compras;
@@ -22,14 +32,25 @@ public:
     QLineEdit *Ncompra;
     QDateEdit *fecha;
     QLineEdit *fact;
+    QLineEdit *nProveedor;
+    QLineEdit *rutProveedor;
     QCheckBox *check;
     QPushButton *guardar;
     QPushButton *revisar;
+    QTableWidget *tabla;
     void closeEvent (QCloseEvent *event);
+    DbManager *database;
+    void setNcompras();
+public slots:
+    void getItem(QTableWidgetItem *item);
+private slots:
+    void on_Guardar_pressed();
 
+    void on_Revisar_pressed();
 
 private:
     Ui::Compras *ui;
+
 };
 
 #endif // COMPRAS_H
