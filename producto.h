228ca032210977b13561750d583f682dd799dad7
Kevin Pizarro <kevin.pizarroa@sansano.usm.cl>
2021-08-09 14:48:46 -0400

    Se agregan las funcionalidades a la sección Productos. Código comentado.

--- producto.h
+++ producto.h
@@ -6,6 +6,16 @@
 #include <QLineEdit>
 #include <QCheckBox>
 #include <QPushButton>
+#include <QWidget>
+#include <QDateEdit>
+#include <QTableView>
+#include <QSqlQueryModel>
+#include <QDebug>
+#include <QSqlRecord>
+#include <QSqlQuery>
+#include <QVector>
+#include <QMessageBox>
+#include <QDate>
 
 namespace Ui {
 class Producto;
@@ -25,21 +35,26 @@ public:
     QLineEdit *desc;
     QLineEdit *oferta;
     QLineEdit *saldo;
-    QLineEdit *compra;
     QLineEdit *codigo;
-    QLineEdit *proov;
+    QLineEdit *prov;
     QCheckBox *Ofcheck;
+    QLineEdit *ganancia;
     QPushButton *guardar;
     QPushButton *borrar;
 
 public slots:
     void change_enabled_oferta();
 
-
 private slots:
     void on_Guardar_clicked();
 
     void on_Eliminar_clicked();
+
+    void getItem();
+
+    void calcprec();
+
+    void calcgan();
 
 private:
     Ui::Producto *ui;
